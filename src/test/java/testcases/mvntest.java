42ad1bb535932c27dfa505680abce86d6d87b8e7
vidhyasri <vidhyasri_Srec@yahoo.co.in>
2018-06-18 23:50:10 -0700

    ddffd

--- src/test/java/testcases/mvntest.java
+++ src/test/java/testcases/mvntest.java
@@ -1,5 +1,11 @@
 package testcases;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -7,9 +13,13 @@ public class mvntest {
 
 		
 		@Test
-		public void test()
+		public void test() throws MalformedURLException
 		{
 			
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.setBrowserName("firefox");
+			System.setProperty("webdriver.gecko.driver","D:\\driver\\geckodriver.exe");
+			WebDriver d= new RemoteWebDriver(new URL ("http://192.168.0.17:5555"),capabilities);
 			SoftAssert softassert= new SoftAssert();
 			softassert.assertTrue(false);
 			System.out.println("hellow wearehappy");
